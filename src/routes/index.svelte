b98ffe9e0198b22992761a125271873555aa24e8
Ivan <75285470+techydad05@users.noreply.github.com>
2022-03-14 18:46:45 -0400

    added video

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,1 @@
-<div class="hero min-h-screen" style="background-image: url(http://localhost:3000/royal-flush-logo-working.png);background-size: contain;background-repeat: no-repeat;">
-    <!-- <div class="hero-overlay bg-opacity-60"></div> -->
-    <div class="hero-content text-center text-neutral-content">
-    </div>
-  </div>
+<video src="//localhost:3000/royalflushlogo_3.mp4" width="100%" autoplay muted></video>
