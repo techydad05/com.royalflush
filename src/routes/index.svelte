9bc22375d3dd5cf3738c6db9cb271f0d38a8f9ac
Ivan <75285470+techydad05@users.noreply.github.com>
2022-03-14 19:03:33 -0400

    Update index.svelte

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,11 @@
-<video src="//localhost:3000/royalflushlogo_3.mp4" width="100%" autoplay muted></video>
+<div class="w-full max-w-screen-lg m-auto">
+    <video src="/royalflushlogo_3.mp4" width="100%" autoplay muted></video>
+</div>
+
+
+<style>
+    video {
+        width: 100% !important;
+        height: auto !important;
+    }
+</style>
