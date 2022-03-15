c86a60f5356298547e96bceeab311d2bdb3df2a1
Ivan <75285470+techydad05@users.noreply.github.com>
2022-03-14 20:54:31 -0400

    adding video

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,5 @@
 <div class="w-full max-w-screen-lg m-auto">
-    <video src="/royalflushlogo_3.mp4" width="100%" autoplay playsinline muted></video>
+    <video src="/royalflushlogo_new.mp4" width="100%" autoplay playsinline muted></video>
 </div>
 
 
