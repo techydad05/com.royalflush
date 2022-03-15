af70cce0fc81b2c672e9e84e6f18e56c3a82c688
Ivan <75285470+techydad05@users.noreply.github.com>
2022-03-14 22:29:58 -0400

    working on stuff

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,22 @@
-<div class="w-full max-w-screen-lg m-auto h-screen align-middle flex bg-[#070c1b]">
+<div class="w-full max-w-screen-lg m-auto h-screen content-center flex flex-col bg-[#070c1b]">
     <video src="/royalflushlogo_new.mp4" width="100%" autoplay playsinline muted></video>
+    <h1 class="text-2xl font-extrabold text-center p-4">See Whats Coming..</h1>
+    <div class="carousel carousel-center">
+    <div class="carousel-item pr-2">
+        <img width="400px" src="/IMG_2497.jpeg" alt="open gummies" />
+    </div> 
+    <div class="carousel-item pr-2">
+        <img width="400px" src="/IMG_2496.jpeg" alt="gummies" />
+    </div>
+    <div class="carousel-item pr-2">
+        <img width="400px" src="/IMG_2505.jpeg" alt="gummies" />
+    </div>
+    <div class="carousel-item pr-2">
+        <img width="400px" src="/IMG_2507.jpeg" alt="gummies" />
+    </div>
+    </div>
 </div>
-
+<h1 class="bg-black text-lg font-bold font-sans text-center">&copy; 2022 royalflush-vip.com All Rights Reserved</h1>
 
 <style>
     video {
