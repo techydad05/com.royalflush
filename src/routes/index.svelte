25b4a95b7db729de4c77f9baf48a88707dee541b
Ivan <75285470+techydad05@users.noreply.github.com>
2022-03-20 12:33:04 -0400

    adding medusa client

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,20 +1,49 @@
-<div class="w-full max-w-screen-lg m-auto h-screen content-center flex flex-col bg-[#070c1b]">
+<script>
+    import { createClient } from  "../client.js";
+    import { onMount } from "svelte";
+    onMount(() => {
+        const client = createClient();
+        console.log(client)
+    });
+    let modalOpen = false;
+    const images = [
+        {
+            src: "/IMG_2496.png",
+            alt: "open royal flush gummies"
+        },
+        {
+            src: "/IMG_2497.png",
+            alt: "closed royal flush gummies"
+        },
+        {
+            src: "/IMG_2505.png",
+            alt: "royal flush strawberry cough disposable"
+        },
+        {
+            src: "/IMG_2507.png",
+            alt: "royal flush wedding cake disposable"
+        },
+    ]
+    const openImage = (e) => {
+        console.log(e.target.src)
+    }
+</script>
+<div class="bg-[#070c1b]">
     <video src="/royalflushlogo_new.mp4" width="100%" autoplay playsinline muted></video>
-    <h1 class="text-2xl font-extrabold text-center p-4">See Whats Coming..</h1>
     <div class="carousel carousel-center">
-    <div class="carousel-item pr-2">
-        <img width="400px" src="/IMG_2497.png" alt="open gummies" />
-    </div> 
-    <div class="carousel-item pr-2">
-        <img width="400px" src="/IMG_2496.png" alt="gummies" />
-    </div>
-    <div class="carousel-item pr-2">
-        <img width="400px" src="/IMG_2505.png" alt="gummies" />
-    </div>
-    <div class="carousel-item pr-2">
-        <img width="400px" src="/IMG_2507.png" alt="gummies" />
-    </div>
-    </div>
+        {#each images as img}
+            <div on:click={(e) => openImage(e)} class="carousel-item">
+            <img src={img.src} width="400px" alt={img.alt} />
+            </div> 
+        {/each}
+      </div>
+</div>
+<div class:modal-open={modalOpen} class="modal">
+  <div class="modal-box relative">
+    <label on:click={() => modalOpen = false} for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+    <h3 class="text-lg font-bold">Congratulations random Interner user!</h3>
+    <p class="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+  </div>
 </div>
 <h1 class="bg-black text-lg font-bold font-sans text-center">&copy; 2022 royalflush-vip.com All Rights Reserved</h1>
 
